e83ac70156db6b212517fe8b6177a1b758687e68
NoSoyFernando <180300352@ucaribe.edu.mx>
2020-12-02 14:03:34 -0500

    completed the scripts for creating users and admins in the database

---
+++ src/scripts/createAdmins.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
+const mariadb = require('../services/mariadb.service.js')
+
+// la entrada debe ser de la siguiente manera
+// usuario email contraseña y el nombre completo
+// administrator joe@PiedPiper.org S3cr3tPass* Roberto Gomez Bolaños
+var stdin_username = process.argv[2];
+var stdin_email = process.argv[3];
+var stdin_hashed = hash.update(process.argv[4]).digest('hex');
+var stdin_fullname = "";
+process.argv.forEach((val, index) => {
+  if(index>4){
+    stdin_fullname = stdin_fullname + val + " ";
+  }
+});
+
+var stmt = "INSERT INTO admins(username, email, fullname, passwd) VALUES(?,?,?,?)";
+var todo = [stdin_username, stdin_email, stdin_fullname, stdin_hashed];
+
+mariadb.getConnection()
+  .then(conn => {
+    console.log("conected with id " + conn.threadId);
+    conn.query("USE uccafe_dihm")
+      .then((results) => {
+        console.log(results); 
+        conn.end(); 
+      })
+
+    conn.query(stmt, todo)
+    .then((results) => {
+      console.log('Registered with id ' + results.insertId);
+    })
+
+  .catch(err => {
+    //handle error
+    conn.end();
+  })
+  conn.end()
+})
---
+++ src/scripts/createUsers.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
+const mariadb = require('../services/mariadb.service.js')
+
+// la entrada debe ser de la siguiente manera
+// usuario contraseña nomnbre apellidos
+// 180300352 S3cr3tPass* Roberto Gomez Bolaños
+var stdin_username = process.argv[2];
+var stdin_hashed = hash.update(process.argv[3]).digest('hex');
+var stdin_name = process.argv[4];
+var stdin_surnames = "";
+
+process.argv.forEach((val, index) => {
+  if(index>4){
+    stdin_surnames = stdin_surnames + val + " ";
+  }
+});
+
+var stmt = "INSERT INTO users(username, name, surnames, passwd) VALUES(?,?,?,?)";
+var todo = [stdin_username, stdin_name, stdin_surnames, stdin_hashed];
+
+mariadb.getConnection()
+  .then(conn => {
+    console.log("conected with id " + conn.threadId);
+    conn.query("USE uccafe_dihm")
+      .then((results) => {
+        console.log(results); 
+        conn.end(); 
+      })
+
+    conn.query(stmt, todo)
+    .then((results) => {
+      console.log('Registered with id ' + results.insertId);
+    })
+
+  .catch(err => {
+    //handle error
+    conn.end();
+  })
+  conn.end()
+})
