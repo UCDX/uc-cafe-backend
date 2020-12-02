9170a5d12805a23aeea59e471acd6906954d0323
Axel Alexis Montejo Lopez <alemontejo.lp@gmail.com>
2020-12-02 18:20:18 -0500

    Added env vars

--- src/scripts/createUsers.js
+++ src/scripts/createUsers.js
@@ -1,3 +1,14 @@
+// Environment variables. Keep loading environment variables at the top
+// or stuff that depends on thery won't work.
+const path =  require('path')
+let envpath = path.join(__dirname, '..', '..', 'etc', '.env')
+let res = require('dotenv').config({ path: envpath })
+if(res.error) {
+  console.error(".env file not found")
+  console.error(res.error.message)
+  process.exit(1)
+}
+
 const crypto = require('crypto');
 const hash = crypto.createHash('sha256');
 const mariadb = require('../services/mariadb.service.js')
